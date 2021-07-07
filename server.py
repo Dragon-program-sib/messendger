1712c8aff91cbb3f34fcfbee12e3caebfcca484b
Dragon <dragon.program.sib@gmail.com>
2021-07-07 20:49:32 +0700

    The work on the file is finished in server.py

--- server.py
+++ server.py
@@ -23,6 +23,63 @@ def hello():
 
 @app.route("/status")
 def status():
-    return "OK"
+    now = datetime.now()
+    return {
+        'status': True,
+        'name': 'Messenger in Python',
+        'time0': time.time(),
+        'time1': time.asctime(),
+        'time2': now,        #dangerous
+        'time3': str(now),
+        'time4': now.isoformat(),
+        'time5': now.strftime('%H:%M:%S')
+    }
+
+@app.route("/send", methods=['POST'])
+def send_message():
+    data = request.json
+
+    # Check data is dict with text & name.
+    if not isinstance(data, dict):
+        return abort(400)
+    if 'text' not in data or 'name' not in data:
+        return abort(400)
+
+    text = data['text']
+    name = data['name']
+
+    # Check text & name are valid strings.
+    if not isinstance(text, str) or not isinstance(name, str):
+        return abort(400)
+    if len(text) == 0 or len(name) == 0:
+        return abort(400)
+    if len(text) > 1000 or len(name) > 100:
+        return abort(400)
+
+    message = {
+        'text': text,
+        'name': name,
+        'time': time.time()
+    }
+    db.append(message)
+
+    return {'ok': True}
+
+
+@app.route("/messages")
+def get_messages():
+    try:
+        after = float(request.args['after'])
+    except:
+        return abort(400)
+
+    result = []
+
+    for message in db:
+        if message['time'] > after:
+            result.append(message)
+
+    return {'messages': result[:100]}
+
 
 app.run() # Запуск сервера.
